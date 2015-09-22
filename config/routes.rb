49cfa2e3533a2c11aa8164cb03c33b943b16df72
Tim Gittos <tim@chromasplosion.com>
2015-09-21 22:32:26 -0500

    Added admin namespace with application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -60,5 +59,11 @@ Rails.application.routes.draw do
   resources :projects do
     resources :tickets
   end
+  
+  namespace :admin do
+    root 'application#index'
+  end
+
+  devise_for :users
 
 end
